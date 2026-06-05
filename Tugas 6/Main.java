0926d3e7f81576d441c8415c3562f3fcc79b445b
Faaizzuhdi <faizzudi4@gmail.com>
2026-06-05 15:34:56 +0700

    Add files via upload

---
+++ Tugas 6/Main.java
@@ -0,0 +1,11 @@
+class Person {
+    String nama;
+    int umur;
+}
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.nama = "Adi";
+        person1.umur = 20;
+    }
+}
